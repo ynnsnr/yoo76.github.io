dee96c0d77a92947c823467077b8eeed7e8776c6
Sebastien Saunier <seb@saunier.me>
2013-09-26 19:29:33 +0200

    Social with font

--- icomoon/lte-ie7.js
+++ icomoon/lte-ie7.js
@@ -6,16 +6,27 @@ window.onload = function() {
 		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-facebook' : '&#x66;',
-			'icon-soundcloud' : '&#x73;',
-			'icon-youtube' : '&#x79;',
-			'icon-twitter' : '&#x74;',
-			'icon-headphones' : '&#x68;',
-			'icon-envelope' : '&#x6d;',
-			'icon-ipod' : '&#x69;'
+			'icon-soundcloud' : '&#xe004;',
+			'icon-youtube' : '&#xe005;',
+			'icon-facebook' : '&#xe002;',
+			'icon-twitter' : '&#xe000;',
+			'icon-envelope' : '&#xe001;',
+			'icon-mobile' : '&#xe003;',
+			'icon-location' : '&#xe006;',
+			'icon-location-2' : '&#xe007;',
+			'icon-location-3' : '&#xe008;',
+			'icon-location-4' : '&#xe009;',
+			'icon-compass' : '&#xe00a;',
+			'icon-compass-2' : '&#xe00b;',
+			'icon-phone' : '&#xe00c;',
+			'icon-phone-2' : '&#xe00d;',
+			'icon-mobile-2' : '&#xe00e;',
+			'icon-star' : '&#xe00f;',
+			'icon-star-2' : '&#xe010;',
+			'icon-star-3' : '&#xe011;'
 		},
 		els = document.getElementsByTagName('*'),
-		i, attr, html, c, el;
+		i, attr, c, el;
 	for (i = 0; ; i += 1) {
 		el = els[i];
 		if(!el) {
